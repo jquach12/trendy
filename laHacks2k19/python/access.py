2dc689cb04f2981227cc4449b8054c9d0689e799
jquach12 <jonq1226@gmail.com>
2019-03-30 11:08:48 -0700

    retrieved totalTraffic

--- laHacks2k19/python/access.py
+++ laHacks2k19/python/access.py
@@ -1,6 +1,12 @@
 import requests
 import json
-r = requests.get("https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=3")
+import ast
+from constants import *
 
-data = json.loads(r.text)
-print(data)
+def retrieve():
+
+	r = requests.get("https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code={}".format(AUS))
+	data = json.loads(r.text)
+	print(data['buckets'][0]['report']['publisher']['traffic']['totalTraffic'])
+
+retrieve()
