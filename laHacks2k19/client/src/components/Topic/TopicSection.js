97008dc1b6579eb0c780413df51430ae5cf98f71
jquach12 <jonq1226@gmail.com>
2019-03-31 05:20:11 -0700

    added 6 articles

--- laHacks2k19/client/src/components/Topic/TopicSection.js
+++ laHacks2k19/client/src/components/Topic/TopicSection.js
@@ -8,7 +8,7 @@ export class TopicSection extends Component {
     super(props);
 
     this.state = { 
-      articles: [['Trump becomes the president', 'https://shop.donaldjtrump.com/'], ['title2', 'https://www.amazon.com/slp/donald-trump-funny/q8uegx2smszp5uq'], ['title3' , 'https://awesomestufftobuy.com/donald-trump-gifts/']]
+      articles: [['Trump cuts all direct assistance to Honduras, El Salvador, Guatemala', "https://www.msn.com/en-us/news/world/trump-cuts-all-direct-assistance-to-honduras-el-salvador-guatemala/ar-BBVpDf5"], ["Trump and GOP target ‘pencil-neck’ Adam Schiff as their post-Mueller villain", "https://www.msn.com/en-us/news/politics/trump-and-gop-target-pencil-neck-adam-schiff-as-their-post-mueller-villain/ar-BBVpeK0"], ["QAnon conspiracy theorists increase their presence at Trump rallies" , "https://www.msnbc.com/the-last-word/watch/qanon-conspiracy-theorists-increase-their-presence-at-trump-rallies-1468233283703"],["Five Key Takeaways from Barr’s Letter to Congress on What’s Next for the Mueller Report", "https://www.msn.com/en-us/news/politics/five-key-takeaways-from-barrs-letter-to-congress-on-whats-next-for-the-mueller-report/ar-BBVp1lM"],["Trump and GOP target ‘pencil-neck’ Adam Schiff as their post-Mueller villain","https://www.msn.com/en-us/news/politics/trump-and-gop-target-pencil-neck-adam-schiff-as-their-post-mueller-villain/ar-BBVpeK0"],["This Footnote to Barr’s Mueller Report Letter Felt Very Random. Perhaps It Wasn’t.","https://slate.com/news-and-politics/2019/03/barr-letter-mueller-report-footnote-russia-coordination.html"]]
     };
   }
   render() {
