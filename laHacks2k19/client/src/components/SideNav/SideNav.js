9c575af4b6afbaa8b925660c4c72607d194b1f3d
Brian Be <bebrian458@gmail.com>
2019-03-30 18:25:52 -0700

    styling first pass

--- laHacks2k19/client/src/components/SideNav/SideNav.js
+++ laHacks2k19/client/src/components/SideNav/SideNav.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './styles.css';
 
 export class SideNav extends Component {
   render() {
     return (
-      <div>
+      <div className="links">
         <button onClick={this.props.selectTrending.bind(this)}>Trending</button>
         <button onClick={this.props.selectCategory.bind(this, "cat1")}>Cat 1</button>
         <button onClick={this.props.selectCategory.bind(this, "cat2")}>Cat 2</button>
